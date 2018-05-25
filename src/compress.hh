3bd2e15c7666f38467119ead5b31ca8634d1a964
Phuntsok Drak-pa <phundrak@phundrak.fr>
2018-05-25 13:22:51 +0200

    removed unused prototype and header

--- src/compress.hh
+++ src/compress.hh
@@ -10,14 +10,9 @@
 #include <vector>
 #include <iostream>
 #include <thread>
-#include <optional>
 
 /// \brief Compression d'une chaine de caractères
 std::vector<std::vector<std::uint32_t>> lzw_compress(std::string &&);
-
-std::optional<std::uint32_t>
-lzw_compress_char(std::vector<std::vector<std::uint32_t>> &,
-                  std::vector<std::uint32_t> &, const char);
 
 /// \brief Wrapper de \ref lzw_compress
 void compress(const std::string &, const char *);
